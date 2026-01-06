ca19adf43d65f4376d8cf35bba1fe6e67977a829
The Italian Developer <theitaliandeveloper@proton.me>
2026-01-06 20:03:16 +0100

    Fix index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
-import pkg from "../package.json" assert { type: "json" };
-export default async function handler(req, res) {
-    res.setHeader("Content-Type", "text/plain");
-    return res.status(200).send(
-      `DNS over HTTPS Proxy by ${pkg.author} (v${pkg.version})`
-    );
+export const config = {
+  runtime: "edge"
+};
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+
+import pkg from "../package.json";
+
+export default function handler() {
+  return new Response(
+    `DNS over HTTPS Proxy by ${pkg.author} (v${pkg.version})`,
+    {
+      headers: {
+        ...corsHeaders,
+        "Content-Type": "text/plain",
+        "Cache-Control": "public, s-maxage=300"
+      }
+    }
+  );
 }
